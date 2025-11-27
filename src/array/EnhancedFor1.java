cf253e4a9d06f28bfeac9ac0e1776830078b940c
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-11-27 19:48:05 +0900

    자바 강의 일단 마저 듣기

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -16,7 +16,7 @@ public class EnhancedFor1 {
         }
          // for-each 문을 사용할 수 없는 경우, 증가하는 index 값 필요
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println("number " + i + "번의 결과는: " + numbers[i]);
+            System.out.println("number uu" + i + "번의 결과는: " + numbers[i]);
         }
     }
 }
---
+++ src/mini_game_practice/Sign_up_login.java
@@ -0,0 +1,9 @@
+package mini_game_practice;
+
+import java.util.Scanner;
+
+public class Sign_up_login {
+    public static void main(String[] args) {
+
+    }
+}
